e1041f31ac062dec6e132a47e6cd6f6db227a0cc
sahil-singh1107 <sahilpython@gmail.com>
2025-03-01 13:50:43 +0530

    added auth

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ClerkProvider } from '@clerk/clerk-react'
 import App from './App.tsx'
 import './index.css'
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing publishable key")
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/" >
+      <App />
+    </ClerkProvider>
   </React.StrictMode>,
 )
 
